b96905e83448e8b5df5c4a68738272a421d4d028
Aseem Timalsina <aseem.tim@gmail.com>
2023-08-02 19:51:47 -0400

    Linear search

---
+++ week3/SearchElement.java
@@ -0,0 +1,15 @@
+package com.aseem.week3.day2;
+
+public class SearchElement {
+    public static void main(String[] args) {
+        int[] userArray = {1,9,4,8,5,7};
+        int numberToSearch = 4;
+        for(int i=0; i<userArray.length; i++) {
+            if(userArray[i] == numberToSearch){
+                System.out.println(i);
+                return;
+            }
+        }
+        System.out.println("Error! No such number in the array");
+    }
+}
