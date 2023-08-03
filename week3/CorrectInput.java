4feadee83fd3cbfce626d62df4ac82fa3583cbf2
Aseem Timalsina <aseem.tim@gmail.com>
2023-08-03 18:50:59 -0400

    Exercises for day 2

---
+++ week3/CorrectInput.java
@@ -0,0 +1,16 @@
+package com.aseem.week3.day2;
+
+import java.util.Scanner;
+
+public class CorrectInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+         System.out.println("Enter either 1 or 2: ");
+        int userInput = scanner.nextInt();
+        while(userInput!=1 && userInput!=2) {
+            System.out.println("Please enter correct value: ");
+            userInput = scanner.nextInt();
+        }
+        System.out.println("Thank you for your input!");
+    }
+}
---
+++ week3/UseOfSplit.java
@@ -0,0 +1,15 @@
+package com.aseem.week3.day2;
+
+import java.util.Scanner;
+
+public class UseOfSplit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a sentence: ");
+        String[] userInput = scanner.nextLine().split(" ");
+
+        for (int i=0; i<userInput.length; i++) {
+            System.out.println(userInput[i]);
+        }
+    }
+}
