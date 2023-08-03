9c5fcc96be73777115a625b55d4fd7f5464243af
Aseem Timalsina <aseem.tim@gmail.com>
2023-08-03 18:20:03 -0400

    Print reversed array

---
+++ week3/DescendingOrder.java
@@ -0,0 +1,14 @@
+package com.aseem.week3.day1;
+
+public class DescendingOrder {
+    public static void main(String[] args) {
+        int[] userArray = {5,3,2,4};
+        int length = userArray.length;
+        int[] reversedArray = new int [length];
+
+        for (int i=length-1; i>=0; i--) {
+            reversedArray[i] = userArray[i];
+            System.out.print(reversedArray[i]);
+        }
+    }
+}
